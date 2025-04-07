6002c43acf6b541586f9c56c353ce72c2c9467ee
Diego-gh <diegohs92@outlook.com>
2025-04-07 19:31:52 -0400

    deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Laugh } from 'lucide-react';
+import { Button } from '@mantine/core';
+
 function App() {
   return (
     <>
-      <div>Hello World!!</div>
+      <div>
+        Hello World!! <Laugh />
+      </div>
+      <Button variant='filled'>Button</Button>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createTheme, MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+
 import App from './App.tsx';
+
+const theme = createTheme({
+  primaryColor: 'red',
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </StrictMode>
 );
